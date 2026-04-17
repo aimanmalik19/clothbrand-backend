d9f4cfd8fd549be78ccc8f586e29280ab0513f4a
Aiman Malik <malikaiman1921@gmail.com>
2026-04-17 15:42:25 +0500

    fix: order model clean

--- models/orderModel.js
+++ models/orderModel.js
@@ -1,38 +1,4 @@
 const mongoose = require('mongoose');
-
-// const orderSchema = new mongoose.Schema({
-//   user: {
-//     type: mongoose.Schema.Types.ObjectId,
-//     ref: 'User',
-//     required: true
-//   },
-//   products: [
-//     {
-//       product: {
-//         type: mongoose.Schema.Types.ObjectId,
-//         ref: 'Product'
-//       },
-//       quantity: {
-//         type: Number,
-//         default: 1
-//       }
-//     }
-//   ],
-//   totalPrice: {
-//     type: Number,
-//     required: true
-//   },
-//   status: {
-//     type: String,
-//     default: 'pending'
-//   },
-//   address: {
-//     type: String,
-//     required: true
-//   }
-// }, { timestamps: true });
-
-
 const orderSchema = new mongoose.Schema({
     name: {
         type: String,
