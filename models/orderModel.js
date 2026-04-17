a9220904370e60072e199dafda33541f94034dbd
Aiman Malik <malikaiman1921@gmail.com>
2026-04-17 15:28:11 +0500

    fix: order model updated

--- models/orderModel.js
+++ models/orderModel.js
@@ -1,35 +1,62 @@
 const mongoose = require('mongoose');
 
+// const orderSchema = new mongoose.Schema({
+//   user: {
+//     type: mongoose.Schema.Types.ObjectId,
+//     ref: 'User',
+//     required: true
+//   },
+//   products: [
+//     {
+//       product: {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: 'Product'
+//       },
+//       quantity: {
+//         type: Number,
+//         default: 1
+//       }
+//     }
+//   ],
+//   totalPrice: {
+//     type: Number,
+//     required: true
+//   },
+//   status: {
+//     type: String,
+//     default: 'pending'
+//   },
+//   address: {
+//     type: String,
+//     required: true
+//   }
+// }, { timestamps: true });
+
+
 const orderSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
-  },
-  products: [
-    {
-      product: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Product'
-      },
-      quantity: {
-        type: Number,
-        default: 1
-      }
+    name: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    items: [
+        {
+            name: String,
+            price: Number,
+            quantity: Number
+        }
+    ],
+    status: {
+        type: String,
+        default: 'pending'
     }
-  ],
-  totalPrice: {
-    type: Number,
-    required: true
-  },
-  status: {
-    type: String,
-    default: 'pending'
-  },
-  address: {
-    type: String,
-    required: true
-  }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Order', orderSchema);
