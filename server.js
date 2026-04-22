5a933dbbe683c5b43d6fbaa5d9266669a697c93a
Aiman Malik <malikaiman1921@gmail.com>
2026-04-22 14:36:27 +0500

    cors fix

--- server.js
+++ server.js
@@ -8,12 +8,10 @@ const productRoutes = require('./routes/productRoutes');
 const orderRoutes = require('./routes/orderRoutes');
 
 const app = express();
-
 app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  origin: '*'
 }));
+
 app.use(express.json());
 
 // MongoDB connect
