847f0bc9d6537657feed2d83ea9fcfd60db525a9
Aiman Malik <malikaiman1921@gmail.com>
2026-04-25 21:28:12 +0500

    fix server

--- server.js
+++ server.js
@@ -7,13 +7,14 @@ const userRoutes = require('./routes/userRoutes');
 const productRoutes = require('./routes/productRoutes');
 const orderRoutes = require('./routes/orderRoutes');
 
+const app = express();  // ← ye missing hai!
 
-const app = express();
 app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
+app.use(express.json());
 
 // MongoDB connect
 mongoose.connect(process.env.MONGO_URI)
